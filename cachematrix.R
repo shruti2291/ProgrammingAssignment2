f8dee277ad8a8f585bf077fecf85f3a3948b3bcd
shruti2291 <shruti2291@gmail.com>
2014-07-27 17:17:39 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix aims at making a cache of a matrix and then later in the program return its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
   m<-NULL
